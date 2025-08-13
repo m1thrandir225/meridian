e555acca543bd32d1918380ff6013fb5af552a71
Зиндл Себастијан <sebastijan.zindl@students.finki.ukim.mk>
2025-08-13 20:24:33 +0200

    fix: refresh-token route in public identity traefik api-gateway

--- internal/identity/application/handlers/dto.go
+++ internal/identity/application/handlers/dto.go
@@ -1,13 +1,48 @@
 package handlers
 
-type UserRegisterRequest struct{}
+type UserResponse struct {
+	UserID    string `json:"user_id"`
+	Username  string `json:"username"`
+	Email     string `json:"email"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+}
 
-type UserRegisterResponse struct{}
+type AuthenticateTokensResponse struct {
+	AccessToken  string `json:"access_token"`
+	TokenType    string `json:"token_type"`
+	ExpiresIn    int64  `json:"expires_in"`
+	RefreshToken string `json:"refresh_token"`
+}
 
-type UserLoginRequest struct{}
+type RegisterUserRequest struct {
+	Username  string `json:"username" binding:"required"`
+	Email     string `json:"email" binding:"required,email"`
+	Password  string `json:"password" binding:"required"`
+	FirstName string `json:"first_name" binding:"required"`
+	LastName  string `json:"last_name" binding:"required"`
+}
 
-type UserLoginResponse struct{}
+type AuthenticateUserRequest struct {
+	Login    string `json:"login" binding:"required"` // Username or Email
+	Password string `json:"password" binding:"required"`
+}
 
-type GetCurrentUserResponse struct{}
+type AuthenticateUserResponse struct {
+	User   UserResponse               `json:"user"`
+	Tokens AuthenticateTokensResponse `json:"tokens"`
+}
 
-type UpdateUserProfile struct{}
+type UpdateProfileRequest struct {
+	Username  *string `json:"username" binding:"omitempty,max=255"`
+	FirstName *string `json:"first_name" binding:"omitempty,max=255"`
+	LastName  *string `json:"last_name" binding:"omitempty,max=255"`
+	Email     *string `json:"email" binding:"omitempty,email"`
+}
+
+type UpdatePasswordRequest struct {
+	NewPassword string `json:"new_password" binding:"required"`
+}
+type RefreshTokenRequest struct {
+	RefreshToken string `json:"refresh_token" binding:"required"`
+}
--- internal/identity/application/handlers/http.go
+++ internal/identity/application/handlers/http.go
@@ -21,54 +21,6 @@ func NewHTTPHandler(userService *services.IdentityService) *HTTPHandler {
 	}
 }
 
-type RegisterUserRequest struct {
-	Username  string `json:"username" binding:"required"`
-	Email     string `json:"email" binding:"required,email"`
-	Password  string `json:"password" binding:"required"`
-	FirstName string `json:"first_name" binding:"required"`
-	LastName  string `json:"last_name" binding:"required"`
-}
-
-type UserResponse struct {
-	UserID    string `json:"user_id"`
-	Username  string `json:"username"`
-	Email     string `json:"email"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-}
-
-type AuthenticateUserRequest struct {
-	Login    string `json:"login" binding:"required"` // Username or Email
-	Password string `json:"password" binding:"required"`
-}
-
-type AuthenticateTokensResponse struct {
-	AccessToken  string `json:"access_token"`
-	TokenType    string `json:"token_type"`
-	ExpiresIn    int64  `json:"expires_in"`
-	RefreshToken string `json:"refresh_token"`
-}
-
-type AuthenticateUserResponse struct {
-	User   UserResponse               `json:"user"`
-	Tokens AuthenticateTokensResponse `json:"tokens"`
-}
-
-type UpdateProfileRequest struct {
-	Username  *string `json:"username" binding:"omitempty,max=255"`
-	FirstName *string `json:"first_name" binding:"omitempty,max=255"`
-	LastName  *string `json:"last_name" binding:"omitempty,max=255"`
-	Email     *string `json:"email" binding:"omitempty,email"`
-}
-
-type UpdatePasswordRequest struct {
-	NewPassword string `json:"new_password" binding:"required"`
-}
-
-type RefreshTokenRequest struct {
-	RefreshToken string `json:"refresh_token" binding:"required"`
-}
-
 // POST /api/v1/register
 func (h *HTTPHandler) handleRegisterRequest(ctx *gin.Context) {
 	var req RegisterUserRequest
